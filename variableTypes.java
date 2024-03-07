0be6954ce816cb8016a49b995756040ecd78768b
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 00:19:43 -0300

    variable Types
    boolean, long, String, int, char, double (for decimals) best practices add D at the value;

---
+++ variableTypes.java
@@ -0,0 +1,19 @@
+
+public class variableTypes  {
+	
+	 
+	
+	boolean flag = true; /* can  be true or false*/
+	char character = 'A'; /* just one character in single quotes*/
+	String word = "Hello"; /* a word in double quotes*/
+	long phone = 131424741111L; /*any long number for example a phone number, followed by the letter L at the end*/
+	double decimalvalues= 245.45D; /* for decimal numbers we use double, it is good practice to add capital D at the end of the value*/
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
