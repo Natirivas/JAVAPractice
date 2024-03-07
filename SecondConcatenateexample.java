4053a0e9bc6656571aa70c9fda126b3d071775c8
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 13:17:18 -0300

    concatenate second example
    concatenating String & int variables.
    Programs read code from left to right, that is why in this case the output shows the int variables separated instead of a sum.

---
+++ SecondConcatenateexample.java
@@ -0,0 +1,12 @@
+
+public class SecondConcatenateexample {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Hello";
+		String str2 = "team";
+			
+		System.out.println(str1 + " "+ str2); /* to add space between string variables "space here" */
+	}
+
+}
