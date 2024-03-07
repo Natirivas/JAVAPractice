84b3834a316d859f25e9109aa1dc478279b8f07e
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 14:06:43 -0300

    nested if else condition

---
+++ nestedifelse.java
@@ -0,0 +1,23 @@
+
+public class nestedifelse {
+
+	public static void main(String[] args) {
+
+		int a = 200;
+		int b = 300;
+		
+	
+	if(a==1000) {  /* to compare values we use == */
+		System.out.println("A is 100 ");
+	}
+	else if(a == 200) {
+		System.out.println("A is 200 ");
+	
+	}
+	else if (a==300) {
+		System.out.println("A is 300 ");
+	}
+	else {
+		System.out.println("Value does not present ");
+	}
+}}
