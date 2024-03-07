8878aa184305df1adbbae63bc3057de8981acc5f
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 13:06:23 -0300

    concatenate
    concatenate example of two strings with space

---
+++ concatenateexample.java
@@ -0,0 +1,11 @@
+
+public class concatenateexample {
+
+	public static void main(String[] args) {
+		String str1 = "Hello";
+		String str2 = "team";
+			
+		System.out.println(str1 + " "+ str2); /* to add space between string variables "space here" */
+	}
+
+}
