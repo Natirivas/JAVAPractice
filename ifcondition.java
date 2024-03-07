5ecc31cdcb536181c1c83693ecefbd1272e20bc5
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 13:58:07 -0300

    if else condition

---
+++ ifcondition.java
@@ -0,0 +1,19 @@
+
+public class ifcondition {
+
+	public static void main(String[] args) {
+
+		
+		int a = 2000;
+		int b = 300;
+	 if( a < 1000)   /* we use if when we want to execute code when a condition that is true*/
+	 {
+		 System.out.println("A is less than 1000 ");
+	 }
+	 else {   /* else will not need any conditions, because it will execute code when the conditions is false*/
+		 System.out.println("A is greater than 1000");
+		 
+	 }
+	}
+
+}
