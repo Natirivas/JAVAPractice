e69ba8d522273d00083f4e2ac1f24535785f9cbf
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 16:33:23 -0300

    while loop
    variable declare outside the loop, then the condition goes in while , statement and finally in this case the increment of the variable ++

---
+++ whileLoop.java
@@ -0,0 +1,15 @@
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		int a = 0;  /* first we start the variable, different from loop that was inside the() */
+while (a <= 10) {  /* the condition goes inside the while()*/
+	
+	System.out.println(a); /*  statement*/
+	a++;    /* after the statement the increment */
+	
+}
+}
+	}
+
+
