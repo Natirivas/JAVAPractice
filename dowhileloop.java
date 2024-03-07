d67afdf2335c093d05772984658af44702429aee
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 16:45:00 -0300

    do while loop
    syntax: 
    do {  *code to be executed goes here
    } while (condition);

---
+++ dowhileloop.java
@@ -0,0 +1,14 @@
+
+public class dowhileloop {
+
+	public static void main(String[] args) {
+
+		int j = 0;    
+		   do {
+			   System.out.println(j);
+		   }  while(j<0);              /*do while loop has one more condition than the while loop*/
+	
+							/* in do while loop the number of iteration is not fixed and we must execute the loop at least once */
+	}					
+
+}
