b31d012d88424b044926e4f36bb7533e37b8bddf
Natalia Rivas <161992123+Natirivas@users.noreply.github.com>
2024-03-07 23:09:08 -0300

    object
    creating an object of a class

---
+++ dog.java
@@ -0,0 +1,25 @@
+
+public class dog {
+	String name= "Valky";
+	String breed = "Bullterrier";
+	int height = 4;
+	
+	public void barking() {
+		
+		System.out.println(name + " is barking ");
+		
+	}
+	
+	public void eating() {
+		System.out.println(name + "is eating ");
+	}
+
+	public static void main(String[] args) {
+		
+		dog dogclass = new dog();  /* all that is presented inside this class it is wrapped in this object*/
+		
+		System.out.println(dogclass.name + " is a  "+ dogclass.breed);
+dogclass.barking();
+	}
+
+}
